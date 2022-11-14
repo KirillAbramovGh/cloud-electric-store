8a597ffe8104c19bdb720b96636fcbbc1e8be2f4
kiab1020 <89879068044slovospb>
2022-11-14 15:55:14 +0300

    initial commit

---
+++ eureka-server/src/test/java/com/electric/store/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.electric.store.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
