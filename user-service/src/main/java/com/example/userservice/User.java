83fca52edbb14b92420cdd1251076786d68574c0
kiab1020 <89879068044slovospb>
2022-11-15 15:46:42 +0300

    add new endpoints to UserController

---
+++ user-service/src/main/java/com/example/userservice/User.java
@@ -0,0 +1,77 @@
+package com.example.userservice;
+
+import java.math.BigInteger;
+import java.util.Objects;
+
+public class User {
+    private BigInteger id;
+    private String name;
+    private String login;
+    private String password;
+
+    public User(){
+
+    }
+
+    public User(BigInteger id, String name, String login, String password) {
+        this.id = id;
+        this.name = name;
+        this.login = login;
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id.equals(user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", login='" + login + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+
+    public BigInteger getId() {
+        return id;
+    }
+
+    public void setId(BigInteger id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
--- user-service/src/main/java/com/example/userservice/controller/UserController.java
+++ user-service/src/main/java/com/example/userservice/controller/UserController.java
@@ -1,21 +1,39 @@
 package com.example.userservice.controller;
 
+import com.example.userservice.User;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
 
-    @GetMapping("/{id}")
-    public ResponseEntity<BigInteger> getUser(@PathVariable(value = "id")BigInteger id){
-        return new ResponseEntity<>(id, HttpStatus.OK);
+    private final List<User> users = new ArrayList(Arrays.asList(new User(new BigInteger("1"),"Lex","LexLogin","LexPass"),
+            new User(new BigInteger("2"),"Den","DenLogin","DenPass")));
+
+    @GetMapping
+    public ResponseEntity<List<User>> getUsers(){
+        return new ResponseEntity<>(users, HttpStatus.OK);
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUser(@PathVariable(value = "id")BigInteger id){
+        return new ResponseEntity<>(users.stream()
+                .filter(u -> u.getId().equals(id)).findFirst().orElse(null), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user){
+        users.add(user);
+        return new ResponseEntity<>(user,HttpStatus.OK);
+    }
+
+
 
 }
